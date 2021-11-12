cbbb45f6e534eea35e86564ee5b51c1edb43bf9e
Eliana Rios <62431281+ElianaJansen@users.noreply.github.com>
2021-11-12 08:49:36 -0300

    agrego slider a la pantalla home
    agrego slider a la pantalla home

--- app/screen/Home.js
+++ app/screen/Home.js
@@ -1,62 +1,131 @@
-import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import React,{ useState} from "react";
+import { View, Text, Image,StyleSheet,Dimensions,StatusBar,SafeAreaView,ScrollView } from "react-native";
 import { Button } from "react-native-elements";
+import AppIntroSlider from 'react-native-app-intro-slider';
 import { useNavigation } from "@react-navigation/native";
 
+
 export default function Home() {
+  const [sliderState, setSliderState] = useState({ currentPage: 0 });
+  const { width, height } = Dimensions.get('window');  
   const navigation = useNavigation();
+  const setSliderPage = (event: any) => {
+  const { currentPage } = sliderState;
+  const { x } = event.nativeEvent.contentOffset;
+  const indexOfNextScreen = Math.floor(x / width);
+    
+    if (indexOfNextScreen !== currentPage) {
+      setSliderState({
+        ...sliderState,
+        currentPage: indexOfNextScreen,
+      });
+    }
+  };
+
+  const { currentPage: pageIndex } = sliderState;
+
   return (
-    <View centerContent={true} style={styles.viewBody}>
-      <Image
-        source={require("../../assets/imagenes/wallpaper_home.png")}
-        resizeMode="contain"
-        style={styles.image}
-      />
-      <Text style={styles.title}>
-        Consulta las mejores ciudades para montar tu negocio
-      </Text>
-      <Text style={styles.description}>
-        ¿Cómo saber si hará buen clima en algún sitio? ¿Deberás prepararte para
-        recibir una jornada inestable? Busca y visualiza los mejores lugares de
-        una forma sencilla, ubicate en el mapa y analiza el clima de tus
-        ciudades favoritas.
-      </Text>
-      <View style={styles.viewBtn}>
-        <Button
-          title="¡Comencemos!"
-          buttonStyle={styles.btnStyle}
-          containerStyle={styles.btnContainer}
-          onPress={() => navigation.navigate("favorites")}
-        />
-      </View>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={{ flex: 1 }}>
+        <ScrollView
+          style={{ flex: 1 }}
+          horizontal={true}
+          scrollEventThrottle={16}
+          pagingEnabled={true}
+          showsHorizontalScrollIndicator={false}
+          onScroll={(event: any) => {
+            setSliderPage(event);
+          }}
+        >
+          <View style={{ width, height }}>
+            <Image
+              source={require('../../assets/imagenes/avatar1_home.png')}
+              resizeMode="contain"
+              style={styles.imageStyle}
+            />
+            <View style={styles.wrapper}>
+              <Text style={styles.header}>Busca y visualiza los mejores lugares de
+                                            una forma sencilla,</Text>
+            </View>
+          </View>
+          <View style={{ width, height }}>
+            <Image
+              source={require('../../assets/imagenes/avatar2_home.png')}
+              resizeMode="contain"
+              style={styles.imageStyle}
+            />
+            <View style={styles.wrapper}>
+              <Text style={styles.header}>Ubicate en el mapa y analiza el clima de tus
+                                          ciudades favoritas</Text>
+            </View>
+          </View>
+          <View style={{ width, height }}>
+            <Image
+              source={require('../../assets/imagenes/avatar3_home.png')}
+              resizeMode="contain"
+              style={styles.imageStyle}
+            />
+            <View style={styles.wrapperBtn}>
+              <Text style={styles.header}>Comienza guardando tus ciudades favoritas</Text>
+                <View style={styles.viewBtn}>
+                  <Button 
+                    title="Ir"
+                    buttonStyle={styles.btnStyle}
+                    containerStyle={styles.btnContainer}
+                    onPress={() => navigation.navigate("favorites")}
+                    />
+                </View>
+            </View>
+          </View>
+        </ScrollView>
+        <View style={styles.paginationWrapper}>
+          {Array.from(Array(3).keys()).map((key, index) => (
+            <View style={[styles.paginationDots, { opacity: pageIndex === index ? 1 : 0.2 }]} key={index} />
+          ))}
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
+
 const styles = StyleSheet.create({
-  viewBody: {
-    marginLeft: 30,
-    marginRight: 30,
+  imageStyle: {
+    height: "50%",
+    width: '100%',
   },
-  image: {
-    height: 280,
-    width: "100%",
-    marginBottom: 40,
-    marginTop: 10,
+  wrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 30,
   },
-  title: {
-    fontWeight: "bold",
-    fontSize: 19,
-    marginTop: 1,
-    textAlign: "center",
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    padding: 10,
+    marginLeft: 10,
   },
-  description: {
-    textAlign: "center",
-    marginBottom: 10,
-    fontSize: 15,
-    marginTop: 15,
+  paginationWrapper: {
+    position: 'absolute',
+    bottom: 200,
+    left: 0,
+    right: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  paginationDots: {
+    height: 10,
+    width: 10,
+    borderRadius: 10 / 2,
+    backgroundColor: '#FB7508',
+    marginLeft: 10,
+  },
+  wrapperBtn: {
+    marginVertical: 50,
   },
   viewBtn: {
-    flex: 1,
     alignItems: "center",
   },
   btnStyle: {
@@ -64,6 +133,6 @@ const styles = StyleSheet.create({
   },
   btnContainer: {
     height: 30,
-    width: "100%",
-  },
+    width: "80%",
+  }, 
 });
