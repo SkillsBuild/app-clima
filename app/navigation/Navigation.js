abe513efadfaf2ee65d06dac30f5b4df21fcd75b
Eliana Rios <62431281+ElianaJansen@users.noreply.github.com>
2021-11-25 15:13:27 -0300

    modifico nombre de stack

--- app/navigation/Navigation.js
+++ app/navigation/Navigation.js
@@ -1,66 +1,88 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Icon } from 'react-native-elements';
+import React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-elements";
 
-import AccountStack from './AccountStack';
-import FavoritesStack from './FavoritesStack';
-import SearchStack from './SearchStack';
+import FavoritesStack from "./FavoritesStack";
+import SearchStack from "./SearchStack";
+import AccountStack from "./AccountStack";
+import CitiesStack from "./CitiesStack";
+import About from "./About";
+
 
 const Tab = createBottomTabNavigator();
 
-export default function Navigation() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="account"
-        tabBarOptions={{
-          inactiveTintColor: '#646464',
-          activeTintColor: '#FB7508',
-        }}
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ color }) => screenOptions(route, color),
-        })}>
-        <Tab.Screen
-          name="account"
-          component={AccountStack}
-          options={{ title: 'Cuenta' }}
-        />
-        
-        <Tab.Screen
-          name="favorites"
-          component={FavoritesStack}
-          options={{ title: 'Favoritos' }}
-        />
-        <Tab.Screen
-          name="search"
-          component={SearchStack}
-          options={{ title: 'Búsqueda' }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+
+export default function Navigation(){
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                initialRouteName= "cities"
+                tabBarOptions= {{
+                    inactiveTintColor: "#646464",
+                    activeTintColor: "#FB7508"
+                }}
+                screenOptions={ ({route}) => ({
+                    tabBarIcon: ({ color }) => screenOptions(route, color)
+                }) }
+            >
+                <Tab.Screen 
+                    name="cities" 
+                    component={CitiesStack} 
+                    options= {{ title: "Ciudades" }}
+                />
+
+                <Tab.Screen 
+                    name="favorites" 
+                    component={FavoritesStack} 
+                    options= {{ title: "Favoritos" }}
+
+                />
+                <Tab.Screen
+                    name="search"
+                    component={SearchStack}
+                    options= {{ title: "Búsqueda" }}
+                />
+                <Tab.Screen
+                    name="account"
+                    component={AccountStack}
+                    options= {{ title: "Mi cuenta" }}
+                />
+                <Tab.Screen
+                    name="about"
+                    component={About}
+                    options= {{ title: "Nosotros" }}
+                />
+
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
 }
 
-function screenOptions(route, color) {
-  let iconName;
+function screenOptions(route,color) {
+    let iconName;
 
-  switch (route.name) {
-    case 'account':
-      iconName = 'home-outline';
-      break;
-    case 'favorites':
-      iconName = 'heart-outline';
-      break;
-    case 'search':
-      iconName = 'magnify';
-      break;
-
-    default:
-      break;
-  }
-  return (
-    <Icon type="material-community" name={iconName} size={22} color={color} />
-  );
+    switch (route.name) {
+        case "favorites":
+            iconName = "heart-outline";
+            break;
+        case "search":
+            iconName = "magnify";
+            break;
+        case "account":
+            iconName = "home-outline";
+            break;
+        case "cities":
+            iconName = "earth";
+            break;
+        case "about":
+            iconName = "account-group";
+            break;
+        default:
+            break;
+    }
+    return(
+        <Icon type="material-community" name={iconName} size={22} color={color} />
+    )
 }
