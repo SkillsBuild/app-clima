b05eefe6c3e648806e785ea991653f2e65e3abec
eliana rios <elianajnsn@gmail.com>
2021-11-25 23:21:33 -0300

    subo cambios

--- app-clima/app/navigation/Navigation.js
+++ app-clima/app/navigation/Navigation.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Icon } from "react-native-elements";
@@ -10,81 +9,77 @@ import AccountStack from "./AccountStack";
 import CitiesStack from "./CitiesStack";
 import About from "./About";
 
-
 const Tab = createBottomTabNavigator();
 
-
-export default function Navigation(){
-    /*Componente principal de navegacion, posee las screen principales de la app, las cuales se podrán navegar
+export default function Navigation() {
+  /*Componente principal de navegacion, posee las screen principales de la app, las cuales se podrán navegar
     a traves de una barra de navegacion colocada en la parte inferior de la app*/
-    return (
-        <NavigationContainer>
-            <Tab.Navigator
-                initialRouteName= "account"
-                tabBarOptions= {{
-                    inactiveTintColor: "#646464",
-                    activeTintColor: "#00a680"
-                }}
-                screenOptions={ ({route}) => ({
-                    tabBarIcon: ({ color }) => screenOptions(route, color)
-                }) }
-            >
-                <Tab.Screen 
-                    name="cities" 
-                    component={CitiesStack} 
-                    options= {{ title: "Ciudades" }}
-                />
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="account"
+        tabBarOptions={{
+          inactiveTintColor: "#646464",
+          activeTintColor: "#FB7508",
+        }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+        })}
+      >
+        <Tab.Screen
+          name="cities"
+          component={CitiesStack}
+          options={{ title: "Ciudades" }}
+        />
 
-                <Tab.Screen 
-                    name="favorites" 
-                    component={FavoritesStack} 
-                    options= {{ title: "Favoritos" }}
-
-                />
-                <Tab.Screen
-                    name="search"
-                    component={SearchStack}
-                    options= {{ title: "Búsqueda" }}
-                />
-                <Tab.Screen
-                    name="account"
-                    component={AccountStack}
-                    options= {{ title: "Mi cuenta" }}
-                />
-                <Tab.Screen
-                    name="about"
-                    component={About}
-                    options= {{ title: "¿Quiénes Somos?" }}
-                />
-
-            </Tab.Navigator>
-        </NavigationContainer>
-    )
+        <Tab.Screen
+          name="favorites"
+          component={FavoritesStack}
+          options={{ title: "Favoritos" }}
+        />
+        <Tab.Screen
+          name="search"
+          component={SearchStack}
+          options={{ title: "Búsqueda" }}
+        />
+        <Tab.Screen
+          name="account"
+          component={AccountStack}
+          options={{ title: "Mi cuenta" }}
+        />
+        <Tab.Screen
+          name="about"
+          component={About}
+          options={{ title: "Nosotros" }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
-function screenOptions(route,color) {
-    let iconName;
+function screenOptions(route, color) {
+  let iconName;
 
-    switch (route.name) {
-        case "favorites":
-            iconName = "heart-outline";
-            break;
-        case "search":
-            iconName = "magnify";
-            break;
-        case "account":
-            iconName = "home-outline";
-            break;
-        case "cities":
-            iconName = "earth";
-            break;
-        case "about":
-            iconName = "account-group";
-            break;
-        default:
-            break;
-    }
-    return(
-        <Icon type="material-community" name={iconName} size={22} color={color} />
-    )
+  switch (route.name) {
+    case "favorites":
+      iconName = "heart-outline";
+      break;
+    case "search":
+      iconName = "magnify";
+      break;
+    case "account":
+      iconName = "home-outline";
+      break;
+    case "cities":
+      iconName = "earth";
+      break;
+    case "about":
+      iconName = "account-group";
+      break;
+    default:
+      break;
+  }
+  return (
+    <Icon type="material-community" name={iconName} size={22} color={color} />
+  );
 }
--- app-clima/app/screens/account/Login.js
+++ app-clima/app/screens/account/Login.js
@@ -1,71 +1,73 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import { StyleSheet, View, ScrollView, Text, Image } from "react-native";
-import { Divider } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
 import Toast from "react-native-easy-toast";
 
 import LoginForm from "../../components/account/LoginForm";
 
-export default function Login(){
-    const toastRef = useRef();
+export default function Login() {
+  const toastRef = useRef();
+  return (
+    <ScrollView>
+      <Image
+        source={require("../../../assets/img/login_logo.png")}
+        resizeMode="contain"
+        style={styles.logo}
+      />
+      <View style={styles.viewContainer}>
+        <LoginForm toastRef={toastRef} />
+        <CreateAccount />
+      </View>
 
-    //Este componente muestra el formulario de Login y la opcion de Registro
+      <View style={{ marginTop: 60 }}>
+        <Image
+          source={require("../../../assets/img/login_background.png")}
+          resizeMode="cover"
+          style={styles.image}
+        />
+      </View>
 
-    return(
-        <ScrollView >
-            <Image
-                source={require("../../../assets/img/logo-weatherApp.png")}
-                resizeMode="contain"
-                style={styles.logo}
-            />
-            <View style={styles.viewContainer}>
-                <LoginForm toastRef={toastRef} />
-                <CreateAccount />
-            </View>
-            <Divider style={styles.divider} />
-            <Toast ref={toastRef} position="center" opacity={0.9} />
-        </ScrollView>
-    )
+      <Toast ref={toastRef} position="center" opacity={0.9} />
+    </ScrollView>
+  );
 }
 
-function CreateAccount(){
-    const navigation = useNavigation();
-
-    return(
-        <Text style={styles.textRegister}>
-            ¿Aun no tienes una cuenta?{" "}
-            <Text
-                onPress={() => navigation.navigate("register")} 
-                style={styles.btnRegister}
-            >
-                Regístrate
-            </Text>
-        </Text>
-    )
+function CreateAccount() {
+  const navigation = useNavigation();
+  return (
+    <Text style={styles.textRegister}>
+      ¿Aún no tiene una cuenta?{" "}
+      <Text
+        style={styles.btnRegister}
+        onPress={() => navigation.navigate("register")}
+      >
+        Regístrate
+      </Text>
+    </Text>
+  );
 }
 
 const styles = StyleSheet.create({
-    logo: {
-        width: "100%",
-        height: 150,
-        marginTop: 20,
-    },
-    viewContainer: {
-        marginRight: 40,
-        marginLeft: 40,
-        backgroundColor: '#FFFFFF',
-    },
-    textRegister: {
-        marginTop: 15,
-        marginLeft: 10,
-        marginRight: 10,
-    },
-    btnRegister: {
-        color: "#1190CB",
-        fontWeight: "bold",
-    },
-    divider: {
-        backgroundColor: "#1190CB",
-        margin: 40,
-    }
-})
+  logo: {
+    width: "100%",
+    height: 150,
+    marginTop: 20,
+  },
+  image: {
+    height: 200,
+    width: "100%",
+  },
+  viewContainer: {
+    marginRight: 40,
+    marginLeft: 40,
+  },
+  textRegister: {
+    marginTop: 15,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  btnRegister: {
+    color: "#FB7508",
+    fontWeight: "bold",
+  },
+});
